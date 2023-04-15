6b252ec99e02d875d4943d7da1d95e9fca24cfab
Samuellekpor <samuellekpor@gmail.com>
2023-04-15 19:10:43 +0000

    Create a reducer

--- src/actions/greetingAction.js
+++ src/actions/greetingAction.js
@@ -1,6 +1,6 @@
 export const fetchGreeting = () => {
   return dispatch => {
-    return fetch('/random-greeting')
+    return fetch('http://localhost:3000/random-greeting')
       .then(response => response.json())
       .then(greeting => dispatch({ type: 'SET_GREETING', payload: greeting }));
   };
